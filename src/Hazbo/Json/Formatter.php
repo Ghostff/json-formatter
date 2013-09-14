0ed785e0770f2c03946dbc503ecf83aaf29abeff
hazbo <hazbo@gmx.com>
2013-09-14 19:27:08 +0100

    Converted tabs 2 spaces

--- src/Hazbo/Json/Formatter.php
+++ src/Hazbo/Json/Formatter.php
@@ -4,47 +4,47 @@ namespace Hazbo\Json;
 
 class Formatter
 {
-	public $json;
+    public $json;
 
-	public function __construct($json = '[]')
-	{
-		$this->json = $json;
-	}
+    public function __construct($json = '[]')
+    {
+        $this->json = $json;
+    }
 
-	public function format($json)
-	{
-		$result    = '';
-		$pos 	   = 0;
-		$strLen    = strlen($json);
-		$indentStr = "\t";
-		$newLine   = "\n";
+    public function format($json)
+    {
+        $result    = '';
+        $pos       = 0;
+        $strLen    = strlen($json);
+        $indentStr = "\t";
+        $newLine   = "\n";
 
-		for ($i = 0; $i < $strLen; $i++) {
-			$char = $json[$i];
-			if ($char == '"') {
-				if (!preg_match('`"(\\\\\\\\|\\\\"|.)*?"`s', $json, $m, null, $i)) {
-					return $json;
-				}
+        for ($i = 0; $i < $strLen; $i++) {
+            $char = $json[$i];
+            if ($char == '"') {
+                if (!preg_match('`"(\\\\\\\\|\\\\"|.)*?"`s', $json, $m, null, $i)) {
+                    return $json;
+                }
 
-				$result .= $m[0];
-				$i 		+= strLen($m[0]) - 1;
-				continue;
-			} else if ($char == '}' || $char == ']') {
-				$result .= $newLine;
-				$pos 	--;
-				$result .= str_repeat($indentStr, $pos);
-			}
+                $result .= $m[0];
+                $i      += strLen($m[0]) - 1;
+                continue;
+            } else if ($char == '}' || $char == ']') {
+                $result .= $newLine;
+                $pos    --;
+                $result .= str_repeat($indentStr, $pos);
+            }
 
-			$result .= $char;
+            $result .= $char;
 
-			if ($char == ',' || $char == '{' || $char == '[') {
-				$result .= $newLine;
-				if ($char == '{' || $char == '[') {
-					$pos ++;
-				}
-				$result .= str_repeat($indentStr, $pos);
-			}
-		}
-		return $result;
-	}
+            if ($char == ',' || $char == '{' || $char == '[') {
+                $result .= $newLine;
+                if ($char == '{' || $char == '[') {
+                    $pos ++;
+                }
+                $result .= str_repeat($indentStr, $pos);
+            }
+        }
+        return $result;
+    }
 }
